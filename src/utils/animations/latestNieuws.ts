8c4a541d9f0aca8fb36ab2650f5ff75413ad5a3f
Daniel Amadi <ama101@outlook.com>
2024-08-21 20:24:56 +0200

    added animations

--- src/utils/animations/latestNieuws.ts
+++ src/utils/animations/latestNieuws.ts
@@ -9,6 +9,6 @@ export const card_container = {
 };
 
 export const card_items = {
-  hidden: { opacity: 0, scale: 0.9 },
-  show: { opacity: 1, scale: 1 },
+  hidden: { opacity: 0 },
+  show: { opacity: 1 },
 };
